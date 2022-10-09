a01053f58b6195cf3c3d27002b324390f28d21b2
matiasgrego <matiasgrego@gmail.com>
2022-10-09 19:34:12 -0300

    agrego Form2 que faltaba

---
+++ Form2.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace TP1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox_dni = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtBox_nombres = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtBox_apellidos = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtBox_email = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.btn_AceptaRegistro = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtBox_contrasena = new System.Windows.Forms.TextBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.txtBox_RepContrasena = new System.Windows.Forms.TextBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBox_dni
+            // 
+            this.txtBox_dni.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBox_dni.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtBox_dni.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtBox_dni.Location = new System.Drawing.Point(676, 87);
+            this.txtBox_dni.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox_dni.MaxLength = 10;
+            this.txtBox_dni.Name = "txtBox_dni";
+            this.txtBox_dni.PlaceholderText = "Tu número de documento";
+            this.txtBox_dni.Size = new System.Drawing.Size(236, 21);
+            this.txtBox_dni.TabIndex = 5;
+            this.txtBox_dni.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(675, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 43);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtBox_nombres
+            // 
+            this.txtBox_nombres.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBox_nombres.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtBox_nombres.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtBox_nombres.Location = new System.Drawing.Point(675, 143);
+            this.txtBox_nombres.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox_nombres.MaxLength = 10;
+            this.txtBox_nombres.Name = "txtBox_nombres";
+            this.txtBox_nombres.PlaceholderText = "Nombres";
+            this.txtBox_nombres.Size = new System.Drawing.Size(236, 21);
+            this.txtBox_nombres.TabIndex = 7;
+            this.txtBox_nombres.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox2.Location = new System.Drawing.Point(674, 132);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(238, 43);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txtBox_apellidos
+            // 
+            this.txtBox_apellidos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBox_apellidos.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtBox_apellidos.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtBox_apellidos.Location = new System.Drawing.Point(676, 198);
+            this.txtBox_apellidos.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox_apellidos.MaxLength = 10;
+            this.txtBox_apellidos.Name = "txtBox_apellidos";
+            this.txtBox_apellidos.PlaceholderText = "Apellidos";
+            this.txtBox_apellidos.Size = new System.Drawing.Size(236, 21);
+            this.txtBox_apellidos.TabIndex = 9;
+            this.txtBox_apellidos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox3.Location = new System.Drawing.Point(675, 187);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(238, 43);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // txtBox_email
+            // 
+            this.txtBox_email.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBox_email.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtBox_email.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtBox_email.Location = new System.Drawing.Point(677, 255);
+            this.txtBox_email.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox_email.MaxLength = 10;
+            this.txtBox_email.Name = "txtBox_email";
+            this.txtBox_email.PlaceholderText = "Correo electrónico";
+            this.txtBox_email.Size = new System.Drawing.Size(236, 21);
+            this.txtBox_email.TabIndex = 11;
+            this.txtBox_email.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox4.Location = new System.Drawing.Point(676, 244);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(238, 43);
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // btn_AceptaRegistro
+            // 
+            this.btn_AceptaRegistro.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(41)))), ((int)(((byte)(30)))));
+            this.btn_AceptaRegistro.FlatAppearance.BorderSize = 0;
+            this.btn_AceptaRegistro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_AceptaRegistro.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_AceptaRegistro.ForeColor = System.Drawing.SystemColors.Window;
+            this.btn_AceptaRegistro.Location = new System.Drawing.Point(675, 410);
+            this.btn_AceptaRegistro.Name = "btn_AceptaRegistro";
+            this.btn_AceptaRegistro.Size = new System.Drawing.Size(238, 44);
+            this.btn_AceptaRegistro.TabIndex = 13;
+            this.btn_AceptaRegistro.Text = "Aceptar";
+            this.btn_AceptaRegistro.UseVisualStyleBackColor = false;
+            this.btn_AceptaRegistro.Click += new System.EventHandler(this.btn_AceptaRegistro_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(41)))), ((int)(((byte)(30)))));
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(2, -6);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(930, 58);
+            this.panel3.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(10, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Online Banking";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackgroundImage = global::TP1.Properties.Resources.hombre_con_laptop;
+            this.flowLayoutPanel1.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 88);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(645, 351);
+            this.flowLayoutPanel1.TabIndex = 15;
+            // 
+            // txtBox_contrasena
+            // 
+            this.txtBox_contrasena.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBox_contrasena.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtBox_contrasena.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtBox_contrasena.Location = new System.Drawing.Point(677, 312);
+            this.txtBox_contrasena.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox_contrasena.MaxLength = 10;
+            this.txtBox_contrasena.Name = "txtBox_contrasena";
+            this.txtBox_contrasena.PlaceholderText = "Contraseña";
+            this.txtBox_contrasena.Size = new System.Drawing.Size(236, 21);
+            this.txtBox_contrasena.TabIndex = 16;
+            this.txtBox_contrasena.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox5.Location = new System.Drawing.Point(676, 301);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(238, 43);
+            this.pictureBox5.TabIndex = 17;
+            this.pictureBox5.TabStop = false;
+            // 
+            // txtBox_RepContrasena
+            // 
+            this.txtBox_RepContrasena.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBox_RepContrasena.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtBox_RepContrasena.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtBox_RepContrasena.Location = new System.Drawing.Point(674, 366);
+            this.txtBox_RepContrasena.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox_RepContrasena.MaxLength = 10;
+            this.txtBox_RepContrasena.Name = "txtBox_RepContrasena";
+            this.txtBox_RepContrasena.PlaceholderText = "Repetí la contraseña";
+            this.txtBox_RepContrasena.Size = new System.Drawing.Size(236, 21);
+            this.txtBox_RepContrasena.TabIndex = 18;
+            this.txtBox_RepContrasena.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox6.Location = new System.Drawing.Point(673, 355);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(238, 43);
+            this.pictureBox6.TabIndex = 19;
+            this.pictureBox6.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(935, 501);
+            this.Controls.Add(this.txtBox_RepContrasena);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.txtBox_contrasena);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.btn_AceptaRegistro);
+            this.Controls.Add(this.txtBox_email);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.txtBox_apellidos);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.txtBox_nombres);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.txtBox_dni);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtBox_dni;
+        private PictureBox pictureBox1;
+        private TextBox txtBox_nombres;
+        private PictureBox pictureBox2;
+        private TextBox txtBox_apellidos;
+        private PictureBox pictureBox3;
+        private TextBox txtBox_email;
+        private PictureBox pictureBox4;
+        private Button btn_AceptaRegistro;
+        private Panel panel3;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label label2;
+        private TextBox txtBox_contrasena;
+        private PictureBox pictureBox5;
+        private TextBox txtBox_RepContrasena;
+        private PictureBox pictureBox6;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TP1
+{
+    internal partial class Form2 : Form
+    {
+         Banco banco;
+
+        public Form2(Banco banco)
+        {
+            this.banco = banco; 
+            InitializeComponent();
+        }
+
+        private void btn_AceptaRegistro_Click(object sender, EventArgs e)
+        {
+            int dni = int.Parse(txtBox_dni.Text);
+
+
+            banco.AltaUsuario(dni, txtBox_nombres.Text, txtBox_apellidos.Text, txtBox_email.Text, txtBox_contrasena.Text);
+
+
+        }
+    }
+}
